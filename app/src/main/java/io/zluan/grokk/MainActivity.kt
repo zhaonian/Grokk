01304067ad9454cb74b0944b09291e2480c08d43
Zhaonian Luan <lzn0830@gmail.com>
2024-12-31 17:07:27 -0800

    Empty Chat architecture

--- app/src/main/java/io/zluan/grokk/MainActivity.kt
+++ app/src/main/java/io/zluan/grokk/MainActivity.kt
@@ -4,19 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.sp
 import dagger.hilt.android.AndroidEntryPoint
 import io.zluan.grokk.ui.theme.GrokkTheme
 
@@ -28,48 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GrokkTheme {
-                App()
+                ChatScreen()
             }
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun App() {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopBar()
-        },
-        content = { innerPadding ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-            ) {
-                Text(
-                    "Hello, world!",
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .systemBarsPadding()
-                )
-            }
-        }
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun CenterAlignedTopBar() {
-    CenterAlignedTopAppBar(
-        title = {
-            Text(text = "Grok [Latest]", style = TextStyle(fontSize = 20.sp))
-        },
-        navigationIcon = {
-
-        },
-        actions = {
-
-        }
-    )
 }
